647e960bec946252eae8d04538618382bf7564dd
hauxun <hautrieu65@gmail.com>
2023-04-01 22:25:35 +0700

    delete bad text

--- qlsv-saga/src/index.tsx
+++ qlsv-saga/src/index.tsx
@@ -10,7 +10,6 @@ import { router } from 'routers';
 import { history } from 'utils';
 import { store } from './app/store';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -39,8 +38,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
